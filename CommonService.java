103f5f96e46e5393164bf863db0279466c95846e
Sneha <64777830+snehacsacharya@users.noreply.github.com>
2020-12-07 00:37:49 +0530

    Add files via upload
    Admin_Common_Controller

---
+++ CommonService.java
@@ -0,0 +1,32 @@
+package com.upgrad.quora.service.business;
+
+import com.upgrad.quora.service.common.AuthTokenParser;
+import com.upgrad.quora.service.dao.UserDao;
+import com.upgrad.quora.service.entity.UserAuthEntity;
+import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import com.upgrad.quora.service.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommonService {
+
+    @Autowired
+    private UserDao userDao;
+
+    public UserEntity userProfile(final String authorization, final String userId) throws AuthorizationFailedException, UserNotFoundException {
+        UserAuthEntity userAuthEntity = userDao.getUserAuthByToken(AuthTokenParser.parseAuthToken(authorization));
+        if (userAuthEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        } else if (userAuthEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to get user details");
+        }
+        UserEntity userEntity = userDao.getUserByUuid(userId);
+        if (userEntity == null) {
+            throw new UserNotFoundException("USR-001", "User with entered uuid does not exist");
+        }
+        return userEntity;
+    }
+
+}
